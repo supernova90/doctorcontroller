9a641f1841bbc832443d4b3abe992a1ecdf125a4
supernova90 <majdiabbasjob@gmail.com>
2023-07-11 10:45:47 +0200

    all done

---
+++ src/main/java/com/wildcodeschool/doctorcontroller/DoctorcontrollerApplication.java
@@ -0,0 +1,56 @@
+package com.wildcodeschool.doctorcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class DoctorcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorcontrollerApplication.class, args);
+	}
+@RequestMapping("/")
+@ResponseBody
+public String index() {
+	return "select your doctor from the list below:"
+	+"<br>"+
+	"<a href='/doctor/1'>Doctor n°1</a>"
+	+"<br>"+
+	"<a href='/doctor/2'>Doctor n°2</a>"
+	+"<br>"+
+	"<a href='/doctor/3'>Doctor n°3</a>"
+	+"<br>"+
+	"<a href='/doctor/4'>Doctor n°4</a>";
+}
+
+@RequestMapping("/doctor/1")
+@ResponseBody
+public String doctorOne() {
+	return "Lord Nele";
+}
+
+@RequestMapping("/doctor/2")
+@ResponseBody
+public String doctorTwo() {
+	return "Sir Aderlard";
+}
+
+
+@RequestMapping("/doctor/3")
+@ResponseBody
+public String doctorThree() {
+	return "Baron Ranald";
+}
+
+
+@RequestMapping("/doctor/4")
+@ResponseBody
+public String doctorFour() {
+	return "Emperor Gerboud";
+}
+
+}
---
+++ src/test/java/com/wildcodeschool/doctorcontroller/DoctorcontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.doctorcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorcontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
